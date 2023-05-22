a384996ab70c270b1c69a500d934b55d51941c8c
aswin-codes <codicon.co@gmail.com>
2023-05-22 19:51:30 +0530

    Chnaged Icons

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -32,180 +34,185 @@ class _HomeState extends State<Home> {
         body: SafeArea(
           child: Stack(
             children: [
-              PageView.builder(
-                scrollDirection: Axis.vertical,
-                controller: _controller,
-                itemCount: _imagePath.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.fromLTRB(24, 10, 10, 10),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          _imagePath[index],
+              GestureDetector(
+                onDoubleTap: () {
+                  setState(() {
+                    isLiked = !isLiked;
+                  });
+                },
+                child: PageView.builder(
+                  scrollDirection: Axis.vertical,
+                  controller: _controller,
+                  itemCount: _imagePath.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.fromLTRB(24, 10, 10, 10),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            _imagePath[index],
+                          ),
+                          fit: BoxFit.cover,
                         ),
-                        fit: BoxFit.cover,
                       ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            SizedBox(
-                              height: 230.h,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Container(
-                                        child: Image.asset(
-                                          "assets/images/profile_pic.png",
-                                          fit: BoxFit.cover,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(
+                                height: 230.h,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          child: Image.asset(
+                                            "assets/images/profile_pic.png",
+                                            fit: BoxFit.cover,
+                                          ),
                                         ),
-                                      ),
-                                      SizedBox(
-                                        width: 10.w,
-                                      ),
-                                      Text(
-                                        "@iamaswin",
+                                        SizedBox(
+                                          width: 10.w,
+                                        ),
+                                        Text(
+                                          "@iamaswin",
+                                          style: GoogleFonts.roboto(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 15.sp,
+                                              color: Colors.white),
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      width: 290.w,
+                                      child: Text(
+                                        "Spinning through the city of angels: LA's iconic roundabout takes traffic for a whirl",
                                         style: GoogleFonts.roboto(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 15.sp,
+                                            fontSize: 12.sp,
+                                            fontWeight: FontWeight.normal,
                                             color: Colors.white),
-                                      )
-                                    ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  SizedBox(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        IconButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                isLiked = !isLiked;
+                                              });
+                                            },
+                                            icon: Icon(
+                                              isLiked
+                                                  ? CupertinoIcons.heart_fill
+                                                  : CupertinoIcons.heart,
+                                              size: 30.h,
+                                              color: Colors.white,
+                                            )),
+                                        Text(
+                                          "31K",
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 12.sp,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
+                                    ),
                                   ),
                                   SizedBox(
-                                    width: 290.w,
-                                    child: Text(
-                                      "Spinning through the city of angels: LA's iconic roundabout takes traffic for a whirl",
-                                      style: GoogleFonts.roboto(
-                                          fontSize: 12.sp,
-                                          fontWeight: FontWeight.normal,
-                                          color: Colors.white),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        SizedBox(height: 10.h,),
+                                        GestureDetector(
+                                          onTap: () {
+                                            
+                                          },
+                                          child: Image.asset("assets/icons/comment.png", height: 30.h, width: 30.h,)),
+                                        Text(
+                                          "2K",
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 12.sp,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        IconButton(
+                                            onPressed: () {
+                                              setState(() {
+                                                isSaved = !isSaved;
+                                              });
+                                            },
+                                            icon: Icon(
+                                              isSaved
+                                                  ? Icons.bookmark
+                                                  : Icons.bookmark_outline,
+                                              size: 30.h,
+                                              color: Colors.white,
+                                            )),
+                                        Text(
+                                          "4K",
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 12.sp,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+
+                                        SizedBox(height: 5.h,),
+                                        GestureDetector(
+                                          onTap: () {
+                                            
+                                          },
+                                          child: Image.asset("assets/icons/share.png", height: 30.h, width: 30.h,)),
+                                        Text(
+                                          "5K",
+                                          style: GoogleFonts.roboto(
+                                              fontSize: 12.sp,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
                                     ),
                                   )
                                 ],
-                              ),
-                            ),
-                            Column(
-                              children: [
-                                SizedBox(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      IconButton(
-                                          onPressed: () {
-                                            setState(() {
-                                              isLiked = !isLiked;
-                                            });
-                                          },
-                                          icon: Icon(
-                                            isLiked
-                                                ? Icons.thumb_up_alt
-                                                : Icons.thumb_up_alt_outlined,
-                                            size: 30.h,
-                                            color: Colors.white,
-                                          )),
-                                      Text(
-                                        "31K",
-                                        style: GoogleFonts.roboto(
-                                            fontSize: 12.sp,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(
-                                            Icons.comment,
-                                            size: 30.h,
-                                            color: Colors.white,
-                                          )),
-                                      Text(
-                                        "2K",
-                                        style: GoogleFonts.roboto(
-                                            fontSize: 12.sp,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      IconButton(
-                                          onPressed: () {
-                                            setState(() {
-                                              isSaved = !isSaved;
-                                            });
-                                          },
-                                          icon: Icon(
-                                            isSaved
-                                                ? Icons.bookmark
-                                                : Icons.bookmark_outline,
-                                            size: 30.h,
-                                            color: Colors.white,
-                                          )),
-                                      Text(
-                                        "4K",
-                                        style: GoogleFonts.roboto(
-                                            fontSize: 12.sp,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      IconButton(
-                                          onPressed: () {
-                                            setState(() {
-                                              isShared = !isShared;
-                                            });
-                                          },
-                                          icon: Icon(
-                                            isShared
-                                                ? Icons.share
-                                                : Icons.share_outlined,
-                                            size: 30.h,
-                                            color: Colors.white,
-                                          )),
-                                      Text(
-                                        "5K",
-                                        style: GoogleFonts.roboto(
-                                            fontSize: 12.sp,
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold),
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  );
-                },
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
               Container(
                 margin: EdgeInsets.fromLTRB(24, 10, 0, 0),
